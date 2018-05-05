91bfce523eddd4fa80453b7f0030e0adc9819fd3
zhanhb <6323014+zhanhb@users.noreply.github.com>
2018-05-05 23:44:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-;(function () {
-    var proto = Array.prototype, sort = proto.sort, map = proto.map, splice = proto.splice,
-        concat = proto.concat;
-    proto.sort = function (compareFunction) {
-        let self = this;
-        if (typeof compareFunction !== 'function') {
-            return sort.apply(self, arguments);
-        }
-        self = map.call(self, (item, index) => [item, index]);
-        splice.call(this, concat.call([0, self.length], map.call(sort.call(self, (a, b) => compareFunction(a[0], b[0]) || a[1] - b[1]), x => x[0])));
-        return this;
+;(function (Array) {
+    var proto = Array.prototype, sort = proto.sort, map = proto.map;
+
+    function wrap(item, index) {
+        return [item, index];
     }
-})();
+
+    function newCmp(fn) {
+        return function (a, b) {
+            return fn(a[0], b[0]) || a[1] - b[1];
+        };
+    }
+
+    if (!function () {
+            var expando = +new Date() + '';
+            return expando.split('').sort(function () {
+                return 0;
+            }).join('') === expando;
+        }()) {
+        proto.sort = function (cmp) {
+            var obj = Object(this), tmp, sorted;
+            if (typeof cmp === 'function') {
+                sorted = sort.call(map.call(obj, wrap), newCmp(cmp));
+                // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
+                // FIXED [,,,1,2,3].sort(()=>0);
+                for (var i = 0, length = sorted.length; i < length; ++i) {
+                    (tmp = sorted[i]) ? (obj[i] = tmp[0]) : (delete obj[i]);
+                }
+                return obj;
+            }
+            // TODO not implements when compareFunction is undefined
+            return sort.apply(obj, arguments);
+        };
+    }
+
+})(Array);
