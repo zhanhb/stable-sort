ec83e1b00a30e6464d07c6916720ff5a2bb50dec
zhanhb <6323014+zhanhb@users.noreply.github.com>
2018-07-18 13:41:27 +0800

    Remove call of Array.prototype.map

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 ;(function (Object, Array, String) {
-    var proto = Array.prototype, sort = proto.sort, map = proto.map, call = wrap.call.bind(wrap.call),
-        apply = wrap.apply.bind(wrap.apply);
+    var proto = Array.prototype, sort = proto.sort, call = newCmp.call.bind(newCmp.call),
+            apply = newCmp.apply.bind(newCmp.apply);
 
-    function wrap(item, index) {
-        return [item, index];
+    function wrap(obj) {
+        obj = Object(obj);
+        var len = obj.length >>> 0;
+        var A = new Array(len);
+        for (var k = 0; k < len; ++k) {
+            if (k in obj) {
+                A[k] = [obj[k], k];
+            }
+        }
+        return A;
     }
 
     function newCmp(fn) {
@@ -12,24 +20,26 @@
             return +call(fn, undefined, x[0], y[0]) || x[1] - y[1];
         } : function (x, y) {
             var xString = String(x[0]), yString = String(y[0]);
-            if (xString < yString) return -1;
-            if (yString < xString) return 1;
+            if (xString < yString)
+                return -1;
+            if (yString < xString)
+                return 1;
             return x[1] - y[1];
         };
     }
 
     if (!function () {
-            // longer than 22
-            var expando = 'stable-sort-expando-' + Math.random();
-            return expando.split('').sort(function () {
-                return 0;
-            }).join('') === expando;
-        }()) {
+        // longer than 22
+        var expando = 'stable-sort-expando-' + Math.random();
+        return expando.split('').sort(function () {
+            return 0;
+        }).join('') === expando;
+    }()) {
         proto.sort = function (cmp) {
             var obj = Object(this), tmp, sorted;
             // FIXED Array.from({length:30},(x,i)=>({value:i,toString(){return 'null'}})).sort();
             if (typeof cmp === 'function' || typeof cmp === 'undefined') {
-                sorted = call(sort, call(map, obj, wrap), newCmp(cmp));
+                sorted = call(sort, wrap(obj), newCmp(cmp));
                 // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
                 // FIXED [,,,1,2,3].sort(()=>0);
                 for (var i = 0, length = sorted.length; i < length; ++i) {
