9ce9604ac8803725109a11f40dc0a7741f2ab5e0
zhanhb <6323014+zhanhb@users.noreply.github.com>
2018-07-24 20:37:10 +0800

    Fix sort with Symbol element

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // stable sort
-;(function (Object, Array, String) {
-    var proto = Array.prototype, sort = proto.sort, call = newCmp.call,
+;(function (Object, Array) {
+    var proto = Array.prototype, origin_sort = proto.sort, call = newCmp.call,
             apply = newCmp.apply;
 
     function wrap(obj) {
@@ -18,7 +18,8 @@
     }
 
     function defaultCmp(x, y) {
-        var xString = String(x[0]), yString = String(y[0]);
+        // FIXED [Symbol(),Symbol()].sort()
+        var xString = '' + x[0], yString = '' + y[0];
         if (xString < yString)
             return -1;
         if (yString < xString)
@@ -33,7 +34,24 @@
         } : defaultCmp;
     }
 
-    if (!function () {
+    function sort(cmp) {
+        if (typeof cmp !== 'function' && typeof cmp !== 'undefined'
+                || (typeof this === 'undefined' || this === null)) {
+            // usually a TypeError is thrown
+            return apply.call(origin_sort, this, arguments);
+        }
+        var obj = Object(this), tmp, sorted;
+        // FIXED Array.from({length:30},(x,i)=>({value:i,toString(){return 'null'}})).sort();
+        sorted = call.call(origin_sort, wrap(obj), newCmp(cmp));
+        // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
+        // FIXED [,,,1,2,3].sort(()=>0);
+        for (var i = 0, length = sorted.length; i < length; ++i) {
+            (tmp = sorted[i]) ? (obj[i] = tmp[0]) : (delete obj[i]);
+        }
+        return obj;
+    }
+
+    (function () {
         var expando = 'stable-sort-expando';
         // IE 9 - Edge 13 have stable sorts for arrays with < 512 elements
         // https://stackoverflow.com/questions/3026281/array-sort-sorting-stability-in-different-browsers
@@ -42,23 +60,6 @@
         return expando.split('').sort(function (a, b) {
             return (a === 'o') - (b === 'o');
         }).join('') === expando.replace(/o/g, '') + expando.replace(/[^o]/g, '');
-    }()) {
-        proto.sort = function (cmp) {
-            if (typeof cmp !== 'function' && typeof cmp !== 'undefined'
-                    || (typeof this === 'undefined' || this === null)) {
-                // usually a TypeError is thrown
-                return apply.call(sort, this, arguments);
-            }
-            var obj = Object(this), tmp, sorted;
-            // FIXED Array.from({length:30},(x,i)=>({value:i,toString(){return 'null'}})).sort();
-            sorted = call.call(sort, wrap(obj), newCmp(cmp));
-            // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
-            // FIXED [,,,1,2,3].sort(()=>0);
-            for (var i = 0, length = sorted.length; i < length; ++i) {
-                (tmp = sorted[i]) ? (obj[i] = tmp[0]) : (delete obj[i]);
-            }
-            return obj;
-        };
-    }
+    }()) || (proto.sort = sort);
 
-})(Object, Array, String);
+})(Object, Array);
