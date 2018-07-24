410d1afe4472dc5231112648bdcbe69e030499e6
zhanhb <6323014+zhanhb@users.noreply.github.com>
2018-07-24 19:31:43 +0800

    call origin sort if this is undefined or null

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // stable sort
 ;(function (Object, Array, String) {
     var proto = Array.prototype, sort = proto.sort, call = newCmp.call,
-        apply = newCmp.apply;
+            apply = newCmp.apply;
 
     function wrap(obj) {
         obj = Object(obj);
@@ -37,25 +37,27 @@
         var expando = 'stable-sort-expando';
         // IE 9 - Edge 13 have stable sorts for arrays with < 512 elements
         // https://stackoverflow.com/questions/3026281/array-sort-sorting-stability-in-different-browsers
-        for (var i = 0; i < 5; ++i) expando += expando;
+        for (var i = 0; i < 5; ++i)
+            expando += expando;
         return expando.split('').sort(function (a, b) {
             return (a === 'o') - (b === 'o');
         }).join('') === expando.replace(/o/g, '') + expando.replace(/[^o]/g, '');
     }()) {
         proto.sort = function (cmp) {
+            if (typeof cmp !== 'function' && typeof cmp !== 'undefined'
+                    || (typeof this === 'undefined' || this === null)) {
+                // usually a TypeError is thrown
+                return apply.call(sort, this, arguments);
+            }
             var obj = Object(this), tmp, sorted;
             // FIXED Array.from({length:30},(x,i)=>({value:i,toString(){return 'null'}})).sort();
-            if (typeof cmp === 'function' || typeof cmp === 'undefined') {
-                sorted = call.call(sort, wrap(obj), newCmp(cmp));
-                // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
-                // FIXED [,,,1,2,3].sort(()=>0);
-                for (var i = 0, length = sorted.length; i < length; ++i) {
-                    (tmp = sorted[i]) ? (obj[i] = tmp[0]) : (delete obj[i]);
-                }
-                return obj;
+            sorted = call.call(sort, wrap(obj), newCmp(cmp));
+            // FIXED Array.prototype.sort.call({0:1,1:0,get length(){return 2}},(x,y)=>x-y)
+            // FIXED [,,,1,2,3].sort(()=>0);
+            for (var i = 0, length = sorted.length; i < length; ++i) {
+                (tmp = sorted[i]) ? (obj[i] = tmp[0]) : (delete obj[i]);
             }
-            // usually a TypeError is thrown
-            return apply.call(sort, obj, arguments);
+            return obj;
         };
     }
 
